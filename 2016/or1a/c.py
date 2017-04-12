22cb8330072b7cac22d08ffcf3e88b3fc6c918f7
x1- <viva008@gmail.com>
2017-04-12 18:38:02 +0900

    updated 2016 or1a-c

--- 2016/or1a/c.py
+++ 2016/or1a/c.py
@@ -6,17 +6,44 @@ Problem C. BFFs
 """
 
 def calc(N, fs, occupied, i, num):
+    print(i, occupied)
     if num == N:
         return num
-    # j = (i - N) if i >= N else i
-    j = i
-    bff = fs[j] - 1
-    if occupied[bff] == 2:
-        return num
-    occupied[j] += 1
+
+    bff = fs[i] - 1
+    if occupied[bff] == 1:
+        return num + 1
+    elif occupied[bff] == 2:
+        print("occupied[bff]", bff, i, fs[bff])
+        if i == (fs[bff] - 1):
+            print("i == bff", i, fs[bff])
+            f = search(N, fs, i)
+            print("search", f)
+            if len(f) == 0:
+                return num
+            else:
+                for sf in f:
+                    print("26: for sf in f", sf, occupied[sf])
+                    if occupied[sf] == 0:
+                        occupied[i] += 1
+                        occupied[sf] += 1
+                        return calc(N, fs, occupied, sf, num + 1)
+                    elif occupied[sf] == 1:
+                        return num + 1
+        else:
+            return 0
+    occupied[i] += 1
     occupied[bff] += 1
-    print(i, occupied)
     return calc(N, fs, occupied, bff, num + 1)
+
+
+def search(N, fs, i):
+    found = []
+    for k in xrange(N):
+        if (fs[k] - 1) == i:
+            found.append( k )
+
+    return found
 
 
 #----------------------------
@@ -39,7 +66,7 @@ for t in xrange( T ):
     mx = None
     for i in xrange(N):
         occupied = [ 0 for _ in xrange(N) ]
-        c = calc(N, fs, occupied, i, 1)
+        c = calc(N, fs, occupied, i, 0)
         print(i, c)
         if mx is None:
             mx = c
